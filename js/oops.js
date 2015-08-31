7a0e58beb7e148a740814deca3e81173f8caa5c9
JCloudYu <j.cloud.yu@gmail.com>
2015-09-01 01:47:42 +0800

    1. Add oops.js 2. Collect all original apis into oops.util

---
+++ js/oops.js
@@ -0,0 +1,41 @@
+(function($, window) {
+	"use strict";
+
+	var
+	prev = window.oops || null,
+	curr = function(){};
+	curr.prev	= function(){ return prev; };
+	curr.typing = {
+		isObject: function( value, strict ){
+			var result = ( typeof value === 'object' );
+
+        	strict = strict || false;
+        	result = result || ( strict ? false : (typeof value === 'function'));
+        	result = result && ( strict ? !Array.isArray(value) : true );
+
+			return !!( value && result );
+		},
+		isCallable: function( value ){
+			return (typeof value === 'function');
+		},
+		isArray: Array.isArray,
+		isString: function( value ) { return (typeof value === 'string'); }
+	};
+	curr.core	= {
+		expand: function( target, source, overwrite ){
+			overwrite = overwrite || false;
+
+			if ( !curr.typing.isObject( source ) || !curr.typing.isObject( target ) ) return;
+
+			for ( var prop in source )
+			{
+				if ( !source.hasOwnProperty( prop ) ) continue;
+				if ( !overwrite && target.hasOwnProperty( prop ) ) continue;
+
+				target[ prop ] = source[ prop ];
+			}
+		}
+	};
+
+	window.oops = curr;
+})(jQuery, window);
---
+++ js/oops.util.js
@@ -0,0 +1,53 @@
+(function () {
+	"use strict";
+
+	oops.util = oops.util || {};
+	oops.core.expand( oops.util, {
+		htmlTextSize: function( text, fontSize, parent ) {
+
+			var	dummy = document.createElement('div'),
+				attr  = { fontSize: fontSize + 'px', padding:'0', position:'absolute', lineHeight:'1', visibility:'hidden' };
+
+			parent	= parent || document.body;
+
+			for(var p in attr) dummy.style[p]= attr[p];
+
+			dummy.appendChild(document.createTextNode(text));
+			parent.appendChild(dummy);
+			var fs = [dummy.offsetWidth,dummy.offsetHeight];
+			parent.removeChild(dummy);
+
+			return fs;
+		},
+		lockScroll: function( lockKey ) {
+			if (window.addEventListener) // older FF
+				window.addEventListener('DOMMouseScroll', oops.util.eventEater, false);
+			window.onwheel = oops.util.eventEater; // modern standard
+			window.onmousewheel = document.onmousewheel = oops.util.eventEater; // older browsers, IE
+			window.ontouchmove  = oops.util.eventEater; // mobile
+			document.onkeydown  = __EAT_SCROLL_KEYS;
+		},
+		releaseScroll: function() {
+			if ( window.removeEventListener ) window.removeEventListener('DOMMouseScroll', oops.util.eventEater, false);
+			window.onmousewheel = document.onmousewheel = null;
+			window.onwheel = null;
+			window.ontouchmove = null;
+			document.onkeydown = null;
+		},
+		eventEater: function(e) {
+			e = e || window.event;
+			if ( e.preventDefault ) e.preventDefault();
+			e.returnValue = false;
+		}
+	}, true);
+
+
+
+	var __scrollKeys = {37: 1, 38: 1, 39: 1, 40: 1};
+	function __EAT_SCROLL_KEYS(e) {
+		if ( !__scrollKeys [e.keyCode] ) return;
+
+		oops.util.eventEater( e );
+		return false;
+	}
+})();
