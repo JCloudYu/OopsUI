eebdbf720d57c818c02797aabfbed4a7e53b5533
JCloudYu <j.cloud.yu@gmail.com>
2015-11-13 10:17:44 +0800

    Add app extension that provides basic skeleton of single-paged application execution environment

---
+++ js/oops.app-jquery.js
@@ -0,0 +1,254 @@
+/**
+ * Created by JCloudYu on 11/11/15.
+ */
+(function( $ ) {
+	"use strict";
+
+	// region [ constants ]
+	var RUN_STATE = {
+		BOOT: 0,
+		RUN: 1,
+		WAIT: 2
+	};
+	// endregion
+
+
+
+	var
+	viewport	= null,
+	instMap		= {},
+	instances	= {},
+	execState	= RUN_STATE.BOOT,
+	dungling	= {
+		pause: function( target ) {
+
+				if ( !target )
+				{
+					oops.util.each( instances, function( inst ){
+						if ( !inst.pause ) return;
+						inst.pause();
+					});
+
+					execState = RUN_STATE.WAIT;
+					$( oops.app ).trigger( "paused" );
+				}
+				else
+				{
+					var inst = instMap[ target ] || instances[ target ] || null;
+					if ( !inst || !oops.typing.isCallable(inst.pause) ) return;
+
+					inst.pause();
+				}
+			},
+			resume: function( target ) {
+
+				if ( !target )
+				{
+					oops.util.each( instances, function( inst ){
+						if ( !inst.resume ) return;
+						inst.resume();
+					});
+
+					execState = RUN_STATE.RUN;
+					$( oops.app ).trigger( "resumed" );
+				}
+				else
+				{
+					var inst = instMap[ target ] || instances[ target ] || null;
+					if ( !inst || !oops.typing.isCallable(inst.resume) ) return;
+
+					inst.resume();
+				}
+			}
+	};
+
+
+
+	oops.app = oops.app || {};
+	oops.core.expand( oops.app, {
+		initiate: function(){
+			oops.app.initiate = ___DO_NOTHING;
+			oops.util.each( instances, function( inst ){
+				if ( !inst.initiate ) return;
+				oops.async(function(){inst.initiate()});
+			});
+
+			execState = RUN_STATE.RUN;
+		},
+		instantiate: function( instanceId, generator ){
+			if ( oops.typing.isCallable(instanceId) )
+			{
+				generator = instanceId;
+				instanceId = undefined;
+			}
+
+			if ( !oops.typing.isCallable( generator ) ) return undefined;
+
+			var kernelCtrl	= {}, inst;
+			if ( !( inst = generator(kernelCtrl) ) )
+				throw "Invalid instance object is returned!";
+
+
+			var uniqueId = oops.util.randomStr( 32 );
+			while( instances[uniqueId] ) uniqueId = oops.util.randomStr( 32 );
+			instances[ uniqueId ] = inst;
+
+
+
+			oops.core.expand( kernelCtrl, ___KERNEL_OPERATOR( uniqueId ) );
+
+			var instId = instanceId || null;
+			if ( instId == "external" ) {
+				console.log( "\"external\" is a reserved keyword! Ignoring..." );
+				instId = null;
+			}
+			if ( instId ) instMap[ instId.toString() ] = inst;
+
+			if ( execState > RUN_STATE.BOOT && inst.initiate )
+				oops.async(function(){inst.initiate()});
+
+			return ___INST_WRAPPER( inst );
+		},
+		tool: {}
+	}, true );
+	oops.core.expand( oops.app.tool, {
+		layoutUI:function(){
+			var viewportWidth	= $( window ).width(),
+			viewportHeight	= $( window ).height();
+
+			viewport = {
+				width: viewportWidth,
+				height: viewportHeight,
+				ratio: viewportWidth / viewportHeight,
+				ratioInv: viewportHeight / viewportWidth
+			};
+
+			$( '.stage' ).each(function(){
+				var
+				self = $( this ),
+				baseWidth	= self.data( 'base-width' ) || viewport.width,
+				baseHeight	= Math.ceil( baseWidth * viewport.ratioInv ),
+				scaleRatio	= viewport.width / baseWidth;
+
+				self.css({
+					width:	baseWidth,
+					height:	baseHeight,
+					webkitTransformOrigin: "0 0 0",
+					transformOrigin: "0 0 0",
+					webkitTransform: oops.string.strtr( 'scale3d( :scale, :scale, 1 )', { ':scale':scaleRatio  } ),
+					transform: oops.string.strtr( 'scale3d( :scale, :scale, 1 )', { ':scale':scaleRatio  } )
+				})
+				.attr({
+					'data-scale': scaleRatio
+				});
+			});
+		}
+	}, true );
+
+
+
+	function ___INST_WRAPPER( inst ) {
+		var _instance = inst;
+
+		return {
+			message: function( message ){
+				oops.async(function(){
+					if ( !_instance.message ) return;
+					_instance.message( { system:false, content:message, target:null, source:"external" } );
+				});
+			},
+			trigger: function( evtType, evtParam ){
+				oops.async(function(){
+					var
+					evtParam = (oops.typing.isObject(evtParam)) ? evtParam : { param: evtParam },
+					event = { type:evtType, system:false, target:null, source:"external" };
+
+					oops.core.expand( event, evtParam, false );
+
+					if ( !_instance.event ) return;
+					_instance.event( event );
+				});
+			},
+			pause: function(){
+				if ( !_instance.pause ) return;
+				_instance.pause();
+			},
+			resume: function(){
+				if ( !_instance.resume ) return;
+				_instance.resume();
+			}
+		};
+	}
+	function ___KERNEL_OPERATOR( refId ) {
+		return {
+			trigger: function( target, evtType, evtParam ) {
+				if ( arguments.length == 0 ) return;
+				if ( arguments.length == 1 )
+				{
+					evtType = target;
+					evtParam = {};
+					target = null;
+				}
+				else
+				if ( arguments.length == 2 )
+				{
+					evtParam = evtType;
+					evtType = target;
+					target = null;
+				}
+
+				oops.async(function(){
+					var
+					evtParam = (oops.typing.isObject(evtParam)) ? evtParam : { param: evtParam },
+					event = { type:evtType, system:false, target:target, source:refId };
+
+					oops.core.expand( event, evtParam, false );
+
+					if ( !target )
+					{
+						oops.util.each( instances, function( inst ){
+							if ( !inst.event ) return;
+							inst.event( event );
+						});
+					}
+					else
+					{
+						var inst = instMap[ target ] || instances[ target ] || null;
+						if ( !inst || !oops.typing.isCallable(inst.event) ) return;
+
+						inst.event( event );
+					}
+				});
+			},
+			message: function( target, message ) {
+				if ( arguments.length == 0 ) return;
+				if ( arguments.length == 1 )
+				{
+					message = target;
+					target = null;
+				}
+
+				oops.async(function(){
+					var msg = { system:false, content:message, target:target, source:refId };
+
+					if ( !target )
+					{
+						oops.util.each( instances, function( inst ){
+							if ( !inst.message ) return;
+							inst.message( msg );
+						});
+					}
+					else
+					{
+						var inst = instMap[ target ] || instances[ target ] || null;
+						if ( !inst || !oops.typing.isCallable(inst.message) ) return;
+
+						inst.message( msg );
+					}
+				});
+			}
+		};
+	}
+	function ___DO_NOTHING(){}
+
+})( jQuery );
